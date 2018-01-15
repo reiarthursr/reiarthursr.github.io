9b86d0d35522af2d811c7265ea67f809697e4e3a
reiarthursr <soaresramalhoarthur@live.com>
2018-01-15 16:05:13 -0200

    torre
    torre

---
+++ Torre/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1516039441,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/torre-sheet0.png",
+		"images/projetil-sheet0.png",
+		"images/inimigo-sheet0.png",
+		"images/inimigo-sheet1.png",
+		"images/base-sheet0.png",
+		"images/parede-sheet0.png",
+		"images/baseinimiga-sheet0.png",
+		"images/explosão-sheet0.png",
+		"images/botão-sheet0.png",
+		"images/botão-sheet1.png",
+		"images/fundo-sheet0.png",
+		"images/créditos-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"pathfind.js"
+	]
+}
